bc82efe82767e8e605fb16e4dd2a5fd58477745e
Ahmed <ahmedfay@hotmail.com>
2015-01-30 12:26:22 -0500

    Update images and css files

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-
     /*
         Background slideshow
     */
@@ -11,8 +10,8 @@ jQuery(document).ready(function() {
     , "/assets/5.jpg"
     , "/assets/6.jpg"
     , "/assets/7.jpg"
+    , "/assets/8.jpg"
     ], {duration: 5000, fade: 700});
-
     /*
         Countdown initializer
     */
@@ -35,7 +34,6 @@ jQuery(document).ready(function() {
                 break;
         }
     });
-
     /*
         Tooltips
     */
@@ -45,38 +43,5 @@ jQuery(document).ready(function() {
     $('.social a.googleplus').tooltip();
     $('.social a.pinterest').tooltip();
     $('.social a.flickr').tooltip();
-
-    /*
-        Subscription form
-    */
-    // $('.success-message').hide();
-    // $('.error-message').hide();
-
-    // $('.subscribe form').submit(function() {
-    //     var postdata = $('.subscribe form').serialize();
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: 'assets/sendmail.php',
-    //         data: postdata,
-    //         dataType: 'json',
-    //         success: function(json) {
-    //             if(json.valid == 0) {
-    //                 $('.success-message').hide();
-    //                 $('.error-message').hide();
-    //                 $('.error-message').html(json.message);
-    //                 $('.error-message').fadeIn();
-    //             }
-    //             else {
-    //                 $('.error-message').hide();
-    //                 $('.success-message').hide();
-    //                 $('.subscribe form').hide();
-    //                 $('.success-message').html(json.message);
-    //                 $('.success-message').fadeIn();
-    //             }
-    //         }
-    //     });
-    //     return false;
-    // });
-
 });
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'contacts#new'
   # root 'welcome#index'
-  # get "launch", to: 'contacts#new', as: :launch
+  get "launch", to: 'contacts#new', as: :launch
   resource :contacts, only: [:new, :create]
 end
