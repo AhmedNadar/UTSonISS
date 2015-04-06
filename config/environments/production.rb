7f0f126be8b131b1079b72abbad2a7bb5eab96ca
Ahmed <ahmedfay@hotmail.com>
2015-04-05 21:25:05 -0400

    More fixes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # config.assets.compile = false
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
 
