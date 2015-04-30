837fef529e2575110c164f12c0e2b2da13526b53
Ahmed Nadar <ahmedfay@hotmail.com>
2015-04-30 09:47:50 -0400

    Update controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
 
-  layout 'pages'
+	layout "pages",    only: [ :idea, :thankyou ]
+	# layout "projects", only: [ :projects, :final, :simi_final ]
+
   def idea
   end
 
   def thankyou
   end
+
+  # def projects
+  # 	# all teams
+  # end
+
+  # def simi_final
+  # 	# 9 teams
+  # end
+
+  # def final
+  # 	# 3 teams
+  # end
 end
